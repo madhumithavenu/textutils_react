d98ffbb96645fef16468f9a720acf944d7f86c83
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 14:52:08 +0530

    additing toggle mode function

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import { useState } from 'react';
 function App() {
 
   const [mode, setMode] = useState('light');
+
+  function toggleMode(){
+    (mode === 'light') ? (setMode('dark')) : (setMode('light'));
+  }
+
   return (
     <>
-      <Navbar title="textUtils" about="About Text Utilities"/>
+      <Navbar title="textUtils" about="About Text Utilities" mode={mode} toggleMode={toggleMode}/>
       <div className='container my-3' >
       <TextForm  heading="Enter the text to analyze"/>
       </div>

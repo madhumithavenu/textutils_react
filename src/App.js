51f015a12bb79e065ad9ac28b9d1ca32feecad5f
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 14:39:39 +0530

    adding use State

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import About from './components/About.jsx';
 import Navbar from './components/Navbar.jsx'
 import TextForm from './components/TextForm.jsx';
+import { useState } from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
   return (
     <>
       <Navbar title="textUtils" about="About Text Utilities"/>

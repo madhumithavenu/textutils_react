519c0d4ba9de3fa7c2617febc231d1fba63b27c4
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 12:07:27 +0530

    Adding button and removing label

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -4,9 +4,12 @@ function TextForm() {
   return (
     <>
     <div className='mb-3'>
+        <h1>{props.heading}</h1>
         <label htmlFor='myBox' className='form-label'>Example textarea</label>
-        <textarea className='form-control' id='myBox' rows="3"></textarea>
-        </div></>
+        <textarea className='form-control' id='myBox' rows="8"></textarea>
+        </div>
+        <button className='btn btn-primary'>Convert to Uppercase</button>
+        </>
   )
 }
 

6aae82d5e742218ff6624748e11d53c0ecb66267
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 15:13:52 +0530

    Adding alert state in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About.jsx';
+import Alert from './components/Alert.jsx';
 import Navbar from './components/Navbar.jsx'
 import TextForm from './components/TextForm.jsx';
 import { useState } from 'react';
@@ -7,6 +8,12 @@ import { useState } from 'react';
 function App() {
 
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null)
+
+  function showAlert(message, type) {
+    setAlert({ msg: message, type: type 
+    })
+  }
 
   function toggleMode(){
     if (mode === 'light') {
@@ -21,6 +28,7 @@ function App() {
   return (
     <>
       <Navbar title="textUtils" about="About Text Utilities" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
       <div className='container my-3' >
       <TextForm  heading="Enter the text to analyze" mode={mode}/>
       </div>

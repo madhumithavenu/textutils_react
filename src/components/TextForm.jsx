e041dce523310c686d446a535d946fa602f3f104
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 12:00:11 +0530

    Adding textform component

---
+++ src/components/TextForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TextForm() {
+  return (
+    <>
+    <div className='mb-3'>
+        <label htmlFor='myBox' className='form-label'>Example textarea</label>
+        <textarea className='form-control' id='myBox' rows="3"></textarea>
+        </div></>
+  )
+}
+
+export default TextForm;

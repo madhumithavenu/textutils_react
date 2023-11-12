e422620002a3fe28184c2126021270a55bf11bdb
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 11:23:53 +0530

    adding navbar and list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
 
+let name = "Madhu"
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className='container'>
+      <h1>Hello {name}</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos totam suscipit recusandae dolorem aliquid incidunt amet eos animi eius obcaecati nihil rerum a quaerat quisquam ipsa perferendis, sapiente velit sequi?</p>
+      </div>
     </>
-
-
   );
 }
 

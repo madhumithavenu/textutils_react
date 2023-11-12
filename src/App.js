6b37d084b6cd94f6cb702e7dd9756dcb55f71fac
Madhumitha <madhumithavenu03@gmail.com>
2023-11-12 11:21:23 +0530

    adding lorem

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Hello</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos totam suscipit recusandae dolorem aliquid incidunt amet eos animi eius obcaecati nihil rerum a quaerat quisquam ipsa perferendis, sapiente velit sequi?</p>
+    </>
+
+
   );
 }
 
